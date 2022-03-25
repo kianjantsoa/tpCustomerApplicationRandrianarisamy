e08c1f07dbf4fd3da33bac469d61d432244980c9
jolivot <kianjantsoajolivot@gmail.com>
2022-03-25 07:53:17 +0300

    Utilisation de datatable

---
+++ src/main/java/mg/itu/tpcustomerapplicationsrandrianarisamy/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpcustomerapplicationsrandrianarisamy.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author jolivot
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
---
+++ src/main/java/mg/itu/tpcustomerapplicationsrandrianarisamy/managedbeans/CustomerBean.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpcustomerapplicationsrandrianarisamy.managedbeans;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import mg.itu.tpcustomerapplicationsrandrianarisamy.Customer;
+import mg.itu.tpcustomerapplicationsrandrianarisamy.session.CustomerManager;
+
+/**
+ *
+ * @author jolivot
+ */
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable {
+    private List<Customer> customerList;
+    
+    @EJB
+    private CustomerManager customerManager;
+
+    /**
+     * Creates a new instance of CustomerBean
+     */
+    public CustomerBean() {
+    }
+    
+    /** 
+   * Retourne la liste des clients pour affichage dans une DataTable 
+   * @return 
+   */  
+    public List<Customer> getCustomers() {
+      if (customerList == null) {
+        customerList = customerManager.getAllCustomers();
+      }
+      System.out.println("taille:" + customerList.size());
+      return customerList;
+    }  
+}
